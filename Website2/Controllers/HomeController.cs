8b3f0fad4e0aac0e9ac060286feeb341c9f301cd
quyen <tranquyenn1945@gmail.com>
2024-03-13 11:11:16 +0700

    Add project files.

---
+++ Website2/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Website2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        // Mỗi controller sẽ tạo ra nhiều action
+        // Mỗi action sẽ tạo ra nhiều view
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult HienThi()
+        {
+            return View();
+        }
+    }
+}
